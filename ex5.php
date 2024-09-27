ee551f635bd7773af04692d08f38f513c3d1480a
unknown <quentin.joubert11800@gmail.com>
2024-09-27 20:45:39 +0200

    Exs faits

--- ex5.php
+++ ex5.php
@@ -6,7 +6,6 @@ $tab[1]=rand(0,99);
 $tab[2]=rand(0,99);
 $tab[3]=rand(0,99);
 $tab[4]=rand(0,99);
-   
 function croissantTab($tab){
     $tabcroissant = sort($tab);
     if ($tab != $tabcroissant){
@@ -15,7 +14,6 @@ function croissantTab($tab){
     else {
         echo "False";
     }
-
 }
 print_r("[$tab[0]],[$tab[1]],[$tab[2]],[$tab[3]],[$tab[4]]");
 echo "<br>";
